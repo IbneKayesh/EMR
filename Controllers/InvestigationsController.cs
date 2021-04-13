91a94d69a9075d364e95746a05a0c772a14ebd27
IBNE KAYESH <ibnekayesh91@outlook.com>
2021-04-13 19:45:55 +0600

    controllers

---
+++ Controllers/InvestigationsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMR.Controllers
+{
+    public class InvestigationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/DRUGS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMR.Models
+{
+    public class DRUGS
+    {
+        public int ID { get; set; }
+        public string DRUGS_NAME { get; set; }
+        public string BRAND_NAME { get; set; }
+    }
+}
