6793594e5f5b07b5022def837e9f0a375df488aa
MithunKumarCSE <mithunkumar71@gmail.com>
2021-04-15 09:42:26 +0600

    ..

--- Models/DRUGS.cs
+++ Models/DRUGS.cs
@@ -17,6 +17,9 @@ namespace EMR.Models
         [Required(ErrorMessage = "{0} is required")]
         public string DRUGS_NAME { get; set; }
 
+        /// <summary>
+        /// //
+        /// </summary>
 
         [Display(Name = "Categorie")]
         [Required(ErrorMessage = "{0} is required")]
